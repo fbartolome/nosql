e5abfed76801e16250ae4f94f12199537443a360
natalia navas <natinavas@gmail.com>
2018-11-11 17:21:03 -0300

    Add generic file path for postgres_public_categories.csv

--- tpe/src/main/java/ar/edu/itba/nosql/Main.java
+++ tpe/src/main/java/ar/edu/itba/nosql/Main.java
@@ -5,23 +5,24 @@ import ar.edu.itba.nosql.io.CSVManager;
 import ar.edu.itba.nosql.models.Trajectory;
 import ar.edu.itba.nosql.models.Venue;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.time.Instant;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            List<Venue> venues = CSVManager.csvToVenues("/Users/franbartolome/Documents/ITBA/Electivas/Paradigma NoSQL/TPE/postgres_public_categories.csv",
+            /* categories csv should be in tpe folder */
+            String path = Paths.get(".").toAbsolutePath().normalize().toString();
+            List<Venue> venues = CSVManager.csvToVenues(path + "/postgres_public_categories.csv",
                     ',');
             TrajectoryCreator trajectoryCreator = new TrajectoryCreator(venues);
             LocalDateTime from = LocalDateTime.of(2018, 10, 10, 12, 0);
             LocalDateTime to = LocalDateTime.of(2018, 10, 11, 12, 0);
             List<Trajectory> trajectories = trajectoryCreator.createTrajectories(2, from, to, 1, 5);
 
-            CSVManager.trajectoriesToCSV(trajectories, "/Users/franbartolome/Documents/ITBA/Electivas/Paradigma NoSQL/TPE/trajectories.csv",
+            CSVManager.trajectoriesToCSV(trajectories, path + "/trajectories.csv",
                     ',');
         } catch (IOException e) {
             e.printStackTrace();
