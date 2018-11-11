7d4fe9b1b37a5a07860d55db174d6169392c948d
Sebastian <sguido@itba.edu.ar>
2018-11-11 17:33:11 -0300

    Trajectory CSV to List

--- tpe/src/main/java/ar/edu/itba/nosql/Main.java
+++ tpe/src/main/java/ar/edu/itba/nosql/Main.java
@@ -4,6 +4,7 @@ import ar.edu.itba.nosql.algorithms.TrajectoryCreator;
 import ar.edu.itba.nosql.io.CSVManager;
 import ar.edu.itba.nosql.models.Trajectory;
 import ar.edu.itba.nosql.models.Venue;
+import ar.edu.itba.nosql.models.Visit;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,15 +15,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         try {
-            List<Venue> venues = CSVManager.csvToVenues("/Users/franbartolome/Documents/ITBA/Electivas/Paradigma NoSQL/TPE/postgres_public_categories.csv",
+            List<Venue> venues = CSVManager.csvToVenues("/Users/seguido/Downloads/postgres_public_categories.csv",
                     ',');
             TrajectoryCreator trajectoryCreator = new TrajectoryCreator(venues);
             LocalDateTime from = LocalDateTime.of(2018, 10, 10, 12, 0);
             LocalDateTime to = LocalDateTime.of(2018, 10, 11, 12, 0);
             List<Trajectory> trajectories = trajectoryCreator.createTrajectories(2, from, to, 1, 5);
 
-            CSVManager.trajectoriesToCSV(trajectories, "/Users/franbartolome/Documents/ITBA/Electivas/Paradigma NoSQL/TPE/trajectories.csv",
+            CSVManager.trajectoriesToCSV(trajectories, "/Users/seguido/ITBA/NoSQL/TPE/nosql/trajectories.csv",
                     ',');
+            List<Trajectory> trajectoriesRetrieved = CSVManager.csvToTrajectories("/Users/seguido/ITBA/NoSQL/TPE/nosql/trajectories.csv",',');
         } catch (IOException e) {
             e.printStackTrace();
         }
