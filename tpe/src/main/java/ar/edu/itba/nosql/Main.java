6f4e1f5c8ac39aab563b288777644710680cc0aa
Francisco Bartolomé <fbartolome18@gmail.com>
2018-11-20 19:44:05 -0300

    refactor main

--- tpe/src/main/java/ar/edu/itba/nosql/Main.java
+++ tpe/src/main/java/ar/edu/itba/nosql/Main.java
@@ -1,13 +1,17 @@
 package ar.edu.itba.nosql;
 
 import ar.edu.itba.nosql.algorithms.DistributedModelling;
+import ar.edu.itba.nosql.algorithms.JanusPopulator;
 import ar.edu.itba.nosql.algorithms.TrajectoryCreator;
 import ar.edu.itba.nosql.algorithms.TrajectoryPrunner;
 import ar.edu.itba.nosql.io.CSVManager;
 import ar.edu.itba.nosql.models.Trajectory;
 import ar.edu.itba.nosql.models.Venue;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,7 +20,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         try {
-
             // Generate random trajectories
             /* categories csv should be in tpe folder */
             String path = Paths.get(".").toAbsolutePath().normalize().toString();
@@ -26,15 +29,14 @@ public class Main {
 
             // TODO ver que el avg largo de trayectorias no sean - max y min
             List<Trajectory> TrajectoriesSS = generateTrajectories(trajectoryCreator, trajectoryPrunner, path, 1000, 5, 5, path + "/trajectoriesSS.csv", path + "/prunnedTrajectoriesSS.csv");
-            List<Trajectory> TrajectoriesSL = generateTrajectories(trajectoryCreator, trajectoryPrunner, path, 1000, 199, 201, path + "/trajectoriesSL.csv", path + "/prunnedTrajectoriesSL.csv");
-            List<Trajectory> TrajectoriesLS = generateTrajectories(trajectoryCreator, trajectoryPrunner, path, 10000, 99, 101, path + "/trajectoriesLS.csv", path + "/prunnedTrajectoriesLS.csv");
-            List<Trajectory> TrajectoriesLL = generateTrajectories(trajectoryCreator, trajectoryPrunner, path, 10000, 199, 201, path + "/trajectoriesLL.csv", path + "/prunnedTrajectoriesLL.csv");
+            List<Trajectory> TrajectoriesSL = generateTrajectories(trajectoryCreator, trajectoryPrunner, path, 1000, 200, 200, path + "/trajectoriesSL.csv", path + "/prunnedTrajectoriesSL.csv");
+            List<Trajectory> TrajectoriesLS = generateTrajectories(trajectoryCreator, trajectoryPrunner, path, 10000, 100, 100, path + "/trajectoriesLS.csv", path + "/prunnedTrajectoriesLS.csv");
+            List<Trajectory> TrajectoriesLL = generateTrajectories(trajectoryCreator, trajectoryPrunner, path, 10000, 200, 200, path + "/trajectoriesLL.csv", path + "/prunnedTrajectoriesLL.csv");
 
-            // Create graph with random trajectories
-            DistributedModelling graphModelling = new DistributedModelling();
-            graphModelling.createVenuesGraph(venues);
-            graphModelling.addTrajectories(TrajectoriesSS, venues);
-            graphModelling.printGraphFeatures();
+            CSVManager.trajectoriesToCSV(TrajectoriesSS, "TrajectoriesSS.csv", ';');
+            CSVManager.trajectoriesToCSV(TrajectoriesSL, "TrajectoriesSL.csv", ';');
+            CSVManager.trajectoriesToCSV(TrajectoriesLS, "TrajectoriesLS.csv", ';');
+            CSVManager.trajectoriesToCSV(TrajectoriesLL, "TrajectoriesLL.csv", ';');
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -51,4 +53,5 @@ public class Main {
         CSVManager.trajectoriesToCSV(trajectories, outputPathPrunned,',');
         return trajectories;
     }
+
 }
